9720e3148a2f3ddc711de2c5bf58ee64f850a246
robaElshazly <roba1818@gmail.com>
2021-03-08 19:06:26 +1000

    search feature

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   # GET /items or /items.json
   def index
-    @items = Item.all
+    @items = Item.search(params[:search])
   end
 
   # GET /items/1 or /items/1.json
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,10 @@ class Item < ApplicationRecord
   belongs_to :user
   belongs_to :category
   has_one_attached :picture
+
+  #Search items query
+  def self.search(search_params) 
+    return all if search_params.blank?
+    where('LOWER(name) LIKE LOWER(?) OR LOWER(description) LIKE LOWER(?)', "%#{search_params}%","%#{search_params}%")
+  end
 end
