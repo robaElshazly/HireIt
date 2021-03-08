f977d9908a08caaf941c8e61840e477f09541323
robaElshazly <roba1818@gmail.com>
2021-03-08 20:23:36 +1000

    creating Address model and set up association with User model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
         protected
       
         def configure_permitted_parameters
-          devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+          devise_parameter_sanitizer.permit(:sign_up, keys: [:name , address_attributes: [:state ,:suburb ,:address ,:postcode ]])
         end
       end
 
