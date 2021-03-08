0bacc98f3215c7985404308d602b5f995b875b0d
robaElshazly <roba1818@gmail.com>
2021-03-08 13:26:19 +1000

    set up aws- set up home page view and controller

--- Gemfile
+++ Gemfile
@@ -53,3 +53,5 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 
+
+gem "aws-sdk-s3", "~> 1.89"
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
+    before_action :set_home_items
     def page
-    end   
+    end 
+    private
+ def set_home_items
+    @items=Item.limit(6).order('id desc')
+ end
 end
