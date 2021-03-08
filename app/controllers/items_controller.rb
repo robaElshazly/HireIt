9c976c758950c052d88f7aa5e74d44b522006da7
robaElshazly <roba1818@gmail.com>
2021-03-08 10:00:25 +1000

    modifying the search bar view

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,15 +23,26 @@ class ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
 
-    respond_to do |format|
-      if @item.save
-        format.html { redirect_to @item, notice: "Item was successfully created." }
-        format.json { render :show, status: :created, location: @item }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @item.errors, status: :unprocessable_entity }
-      end
-    end
+    if @item.save
+      flash[:success] = "Item Created"
+      redirect_to items_path
+  else
+      flash.now[:error] = @item.errors.full_messages.to_sentence
+      set_categories
+      render "new"
+  end
+
+    # respond_to do |format|
+    #   # if @item.save
+    #   #   format.html { redirect_to @item, notice: "Item was successfully created." }
+    #   #   format.json { render :show, status: :created, location: @item }
+    #   #   redirect_to items_path
+    #   # else
+    #   #   # format.html { render :new, status: :unprocessable_entity }
+    #   #   # format.json { render json: @item.errors, status: :unprocessable_entity }
+    #   #   redirect_to "new"
+    #   # end
+    # end
   end
 
   # PATCH/PUT /items/1 or /items/1.json
