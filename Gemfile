10cfcff9c00a5b72a23be84efaa969b6a5f639cd
robaElshazly <roba1818@gmail.com>
2021-03-09 12:37:04 +1000

    extend edit user view with address

--- Gemfile
+++ Gemfile
@@ -55,3 +55,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 
 gem "aws-sdk-s3", "~> 1.89"
+gem 'pagy'
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/show'
   resources :categories
   resources :items
  # devise_for :users
   devise_for :users, controllers: { registrations: 'users/registrations' }
+  resources :users, :only=> [:show]
   root 'home#page' 
 end
