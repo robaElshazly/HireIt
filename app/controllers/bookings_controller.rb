6bc26a50a6e6513208a1d2d9e33ae7290a68a8d6
robaElshazly <roba1818@gmail.com>
2021-03-14 12:44:36 +1000

    update new booking view

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,11 @@
 class BookingsController < ApplicationController
   def new
+    @item=Item.find(params[:item_id])
+    if !@item
+      respond_to do |format|
+        format.html { redirect_to root_path, alert: "resource not found"}
+       end
+    end 
+      
   end
 end
