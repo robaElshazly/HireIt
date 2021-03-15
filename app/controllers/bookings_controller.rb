d9760e4e1f74afc1e975c28851900e41e42dd494
robaElshazly <roba1818@gmail.com>
2021-03-15 12:20:28 +1000

    check URL date exist and no time intersections

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,11 +1,21 @@
 class BookingsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
+  
   def new
-    @item=Item.find(params[:item_id])
-    if !@item
+    @item=Item.find_by(id: params[:item_id])
+    if !@item      #ensure item founf from URL exists
       respond_to do |format|
         format.html { redirect_to root_path, alert: "resource not found"}
        end
+    elsif params[:start_date].blank? || params[:start_date].blank?
+      respond_to do |format|
+        format.html { redirect_to item_path(@item.id), alert: "Choose thime for booking before making a booking"}
+       end
+    elsif intersect_unavailable_dates      #ensure date in the URL does not intersect with unavailable dates in the database
+        respond_to do |format|
+          format.html { redirect_to root_path, alert: "unavailable booking time"}
+        end 
+      
     else
       Stripe.api_key=ENV['STRIPE_SK_TEST']
       @session=Stripe::Checkout::Session.create(
@@ -24,8 +34,8 @@ class BookingsController < ApplicationController
         ],
         payment_intent_data: {metadata: {
           item_id: @item.id,
-          start_date: params[:start_date],
-          end_date: params[:end_date], 
+          start_date:Date.parse(params[:start_date]),
+          end_date: Date.parse(params[:end_date]), 
           customer_user_id: current_user.id, 
           owner_user_id:@item.user_id
         }
@@ -40,5 +50,29 @@ class BookingsController < ApplicationController
   def cancel
   end
   def webhook
+    payment_id=params[:data][:object][:payment_intent]
+    payment=Stripe::PaymentIntent.retrieve(payment_id)
+    item_id=payment.metadata.item_id
+    start_date= payment.metadata.start_date
+    end_date=payment.metadata.end_date 
+    customer_user_id=payment.metadata.customer_user_id
+    owner_user_id=payment.metadata.owner_user_id
+    Booking.create(customer_user_id: customer_user_id,owner_user_id: owner_user_id,item_id: item_id,start_date: start_date,end_date: end_date)
+    render plain: "success"
   end
+
+  private 
+
+  
+  def intersect_unavailable_dates  
+    #intersection query (parameters times intersect any of booking times)
+    start_date=Date.parse(params[:start_date])
+    end_date=Date.parse(params[:end_date])
+    intersection=Booking.where('item_id = ? AND start_date <= ? AND end_date >= ?',params[:item_id], end_date,start_date)
+    if intersection.length >0  #there is an intersection
+      return true
+    else
+      return false
+    end    
+   end
 end
